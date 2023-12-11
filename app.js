ba23e96ae80388e102ac5f42f37a1568ead44efe
VERAPAO <pmnvera1984@gmail.com>
2023-12-10 22:10:10 -0300

    Generé dos funciones pathJoin y appGet, que permiten al ejecutar appGet(rute, fileName) que se generen las rutas pedidas

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
 const port = 3030;
+
+const pathJoin = (fileName) =>
+    path.join(__dirname,`./views/${fileName}.html`);
+
+const appGet = (rute, fileName) => app.get(`${rute}`, (req, res) => {
+    res.sendFile(pathJoin(fileName))
+});
+
+appGet('/','index');
+appGet('/babbage', 'babbage');
+appGet('/berners-lee', 'berners-lee');
+appGet('/clarke', 'clarke');
+appGet('/hamilton', 'hamilton')
+appGet('/hopper', 'hopper')
+appGet('/lovelace', 'lovelace')
+appGet('/turing', 'turing')
+
+
 
 app.listen(port, () => {
     console.log(`localhost:${port}`)
-})
+});
